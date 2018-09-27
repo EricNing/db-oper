2c25e049c7a3ec995a3e07139d2b8b0a4dcfb5a3
Eric NZ <nz@email.com>
2018-09-27 22:45:48 +0800

    cx_Oracle: implement the result sets of cursor.fetchall(fetchone) from list(tuple) to dict using cursor.rowfactory. modified file: database/oracle.py

--- database/oracle.py
+++ database/oracle.py
@@ -28,6 +28,12 @@ class Oracle(object):
             self.logger.error(logger_info)
             return False
 
+    def makedict(self, cursor):
+        cols_name = [c[0] for c in cursor.description]
+        def gendict(*args):
+            return dict(zip(cols_name, args))
+        return gendict
+
     def _connPing(self):
         try:
             self._conn.ping()
@@ -51,6 +57,7 @@ class Oracle(object):
     def _getALL(self, sql):
         self._check_db_conn()
         self._cursor.execute(sql)
+        self._cursor.rowfactory = self.makedict(self._cursor)
         res = self._cursor.fetchall()
         # self._commit()
         return res
@@ -59,6 +66,7 @@ class Oracle(object):
         self._check_db_conn()
         # print self._cursor
         self._cursor.execute(sql)
+        self._cursor.rowfactory = self.makedict(self._cursor)
         res = self._cursor.fetchone()
         # self._commit()
         return res
@@ -71,6 +79,7 @@ class Oracle(object):
     # 获取insert的SQL语句
     def _getInsertSQL(self, table_name, value_dict):
     # The Type of value_dict is dictionary
+    # Example: insert into table_name(col1,col2,..,coln) values(:col1,:col2,..,coln)
         fields = ','.join(value_dict.keys())
         val_fields = ','.join(map(lambda x: ':%s' % x, value_dict.keys()))
         sql = 'insert into %s(%s) values(%s)' % (table_name, fields, val_fields)
@@ -78,8 +87,26 @@ class Oracle(object):
 
     # 插入一条记录
     def _insertOne(self, sql, value):
+    # value : list/tuple/dict
+    # value_dict = {'ins_col1': val1, 'ins_col2': val2,..., 'ins_coln': valn}
+    # Example: cursor.execute('insert into table_name(col1,col2,..,coln) values(:ins_col1,:ins_col2,..,ins_coln)', value_dict)
         self._check_db_conn()
         self._cursor.execute(sql, value)
+
+    # 插入多条记录
+    def _insertMany(self):
+    # value_dict = [{'ins_col1': val_11, 'ins_col2': val_21,..., 'ins_coln': val_n1},{'ins_col_12': val1, 'ins_col2': val_22,..., 'ins_coln': val_n2}]
+    # Example: cursor.execute('insert into table_name(col1,col2,..,coln) values(:ins_col1,:ins_col2,..,ins_coln)', value_dict)
+        self._check_db_conn()
+        self._cursor.executemany(sql, value)
+
+    # 更新操作
+    def _update(self, table_name, set_dict, where_dict):
+        pass
+
+    # 删除操作
+    def _delete(self):
+        pass
 
     # 提交操作
     def _commit(self):
